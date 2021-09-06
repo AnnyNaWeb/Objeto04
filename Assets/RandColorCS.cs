9d71ce5a07a591a86e80d01d3106dd55e383eb96
faelro <rafael.rodrigues500@outlook.com>
2021-09-06 20:36:24 -0300

    Variáveis RandColor e Scene

--- Assets/RandColorCS.cs
+++ Assets/RandColorCS.cs
@@ -1,56 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RandColorCS : MonoBehaviour
 {
-    struct Cube{
+    struct Cube
+    {
         public Vector3 position;
         public Color color;
     }
 
+
     public ComputeShader computeShader;
-    public int iteractions = 50;
-    public int count= 100;
     GameObject[] gameObjects;
-    Cube[] data; 
+    Cube[] data;
     public GameObject modelPref;
- 
-    //botão pra criar o cubo pra executar, pra configurar as cores aleatorias em CPU e GPU a medida que formos clicando
-   //tudo em cpu
-    void OnGUI(){
-        if(data == null){//so usa o create cube se o data tiver vazio
-            if(GUI.Button(new Rect (0,0,100,50), "Create")){
-              createCube();
-            }
-        }
-       
-    }
-//tudo em CPU
-    private void createCube(){
-        //alocar memoria pra esse cubo
-        data = new Cube[count *count]; //100 x 100 cubos
-        gameObjects = new GameObject[count * count];
+    public int iteractions = 50;
 
-        for (int i=0; i<count; i++){
-            float offsetX = (-count/2+i);
-            //dimensao y
-            for(int j =0; j < count; j++){
-                //50 lado negativo e 50 lado positivo do x e mesma coisa pro y, vamos centralizar sabendo que o centro é o 0, dar offset pra começar considerando cubo unitario com 10 cubos centralizados a gnt tem que começar no -5 e vai até o 5 pra centralizar tudo na tela
-                float offsetY = (-count/2+j);
-                Color _color = Random.ColorHSV(); //cor randomica
-                
-                GameObject go = GameObject.Instantiate(modelPref, new Vector3(offsetX* 2.2f, 0, offsetY* 2.2f), Quaternion.identity);
-                go.GetComponent<MeshRenderer>().material.SetColor("_Color", _color); //shader tem o _ na frente
+    [Range(1, 50)]
+    public int count = 2;
 
-                //conversão de indice duas ou mais dimensoes para uma dimensao inferior ou superior no caso bidimensional pra unidimensional
-                gameObjects[i*count + j] = go;
-                data[i*count + j] = new Cube();
-                data[i*count+j].position = go.transform.position;
-                data[i*count+j].color = _color;
-            }
-        }
+    [Range(1, 10)]
+    public float velocidadeMin = 1;
 
+    [Range(1, 10)]
+    public float velocidadeMax = 3;
 
-    }
-}
+    [Range(1, 50)]
+    public int massa = 1;
+
+    public bool cpu = false;
+    public bool gpu = false;
+    public static bool playtime = false;
+
+    public static float totalTime;
+    public Text tempo;
+    private float minutes;
+    private static float seconds = 60;
+    private static float miliseconds = 1000;
+
+    
